8a4a8151abc4fa9f490eb5704dd61a3a79edfa54
ElieOko <elieoko100@gmail.com>
2024-07-30 15:05:16 +0100

    fix:number CPUs

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const {
     getEnvironmentData} = require('worker_threads');
 
 setEnvironmentData("auth",{user:{username:"elieoko"}})
+const numCPUs = require('node:os').availableParallelism();
 if (!isMainThread) {
    console.log("Inside worker")
 } 
 const dataShared = getEnvironmentData('auth')
 console.log(dataShared);
+console.log(numCPUs);
